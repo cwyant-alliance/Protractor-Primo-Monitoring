5648386def33e921fc639c279633166171f25c02
Curtis Wyant <cwyant@orbiscascade.org>
2018-03-21 15:21:56 -0700

    Added logging for PBO checks

--- specs/pbo.js
+++ specs/pbo.js
@@ -3,6 +3,10 @@ var LoginPagePBO = require('../pages/LoginPagePBO');
 var BasePage = require('../pages/BasePage');
 var users = require('../data/users.json');
 
+// logging
+const fs = require('fs');
+var logs = '<?xml version="1.0" encoding="UTF-8" ?><pbo>';
+
 describe('logging in to primo back office', function() {
 
         beforeEach(function() {
@@ -31,12 +35,59 @@ describe('check CP_Alma last run', function() {
     });
 
     it('stage should be completed successfully', function() {
-            expect(PboPage.jobStats('CP_Alma', 1)).toBe('completed successfully');
+          expect(PboPage.jobStats('CP_Alma', 1)).toBe('completed successfully');
     });
 
     it('status should be completed', function() {
           expect(PboPage.jobStats('CP_Alma', 2)).toBe('completed');
     });
+
+    it('should have a start time', function() {
+                PboPage.jobStats('CP_Alma', 3).then(function(text) {
+                expect(text).not.toBe(null);
+                logs += '<cp_alma><start>' + text + '</start>';
+            });
+    });
+
+    it('should have an end time', function() {
+                PboPage.jobStats('CP_Alma', 4).then(function(text) {
+                expect(text).not.toBe(null);
+                logs += '<end>' + text + '</end></cp_alma>';
+            });
+    });
+
+}); // CP_Alma
+
+describe('check Dedup_Frbr last run', function() {
+
+    beforeEach(function() {
+        PboPage.to();
+        PboPage.monitorLink.click();
+        PboPage.jobMonitorLink.click();
+    });
+
+    it('stage should be completed successfully', function() {
+          expect(PboPage.jobStats('Dedup_Frbr', 1)).toBe('completed successfully');
+    });
+
+    it('status should be completed', function() {
+          expect(PboPage.jobStats('Dedup_Frbr', 2)).toBe('completed');
+    });
+
+    it('should have a start time', function() {
+                PboPage.jobStats('Dedup_Frbr', 3).then(function(text) {
+                expect(text).not.toBe(null);
+                logs += '<dedup><start>' + text + '</start>';
+            });
+    });
+
+    it('should have an end time', function() {
+                PboPage.jobStats('Dedup_Frbr', 4).then(function(text) {
+                expect(text).not.toBe(null);
+                logs += '<end>' + text + '</end></dedup>';
+            });
+    });
+
 }); // CP_Alma
 
 describe('check Indexing_and_Hotswapping last run', function() {
@@ -54,6 +105,22 @@ describe('check Indexing_and_Hotswapping last run', function() {
     it('status should be completed', function() {
           expect(PboPage.jobStats('Indexing_and_Hotswapping', 2)).toBe('Completed' || 'Running');
     });
+
+    it('should have a start time', function() {
+                PboPage.jobStats('Indexing_and_Hotswapping', 3).then(function(text) {
+                expect(text).not.toBe(null);
+                logs += '<indexing><start>' + text + '</start>';
+            });
+    });
+
+    it('should have an end time', function() {
+                PboPage.jobStats('Indexing_and_Hotswapping', 4).then(function(text) {
+                expect(text).not.toBe(null);
+                logs += '<end>' + text + '</end></indexing>';
+            });
+    });
+
+
 
 }); // Indexing_and_Hotswapping
 
@@ -84,6 +151,12 @@ describe('check Indexing_and_Hotswapping last run', function() {
           //expect(PboPage.sliceStats().get(8).getText()).toBe('3');
           expect(PboPage.sliceStats().get(9).getText()).toBe('Up');
           expect(PboPage.sliceStats().get(10).getText()).toBe('Synchronised');
+
+          // logging
+          logs += '</pbo>';
+          fs.writeFile('testresults/log.xml', logs, (err) => {
+              if (err) throw err;
+          });
     });
 
 }); // slice status
